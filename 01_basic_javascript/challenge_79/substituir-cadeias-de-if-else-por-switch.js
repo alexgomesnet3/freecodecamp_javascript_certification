57055449f4355b97dd62eb596371e77101f629b5
“Alex <alexgomes@Alex-MacBook-Pro.local>
2022-12-29 19:58:32 -0300

    More challenges

---
+++ 01_basic_javascript/challenge_79/substituir-cadeias-de-if-else-por-switch.js
@@ -0,0 +1,25 @@
+function chainToSwitch(val) {
+  let answer = "";
+  // Altere apenas o código abaixo desta linha
+  switch (val) {
+    case "bob":
+      return answer = "Marley";
+      break;
+    case 42:
+      return answer = "The Answer";
+      break;
+    case 1:
+      return answer = "There is no #1";
+      break;
+    case 99:
+      return answer = "Missed me by this much!";
+      break;
+    case 7:
+      return answer = "Ate Nine";
+      break;
+  }
+  // Altere apenas o código acima desta linha
+  return answer;
+}
+
+chainToSwitch(7);
