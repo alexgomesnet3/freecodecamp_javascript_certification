e6fad09aeb1045e58817f5e9f5f5155a6ad056f1
“Alex <alexgomes@Alex-MacBook-Pro.local>
2023-01-01 17:22:45 -0300

    Challenges 83, 84 and 85

---
+++ 01_basic_javascript/challenge_83/criar-objetos-javascript.js
@@ -0,0 +1,8 @@
+const myDog = {
+  // Altere apenas o código abaixo desta linha
+  "name": "Andy",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Paçoca", "Puf", "Guido"]
+  // Altere apenas o código acima desta linha
+};
